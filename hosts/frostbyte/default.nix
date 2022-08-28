f9e272c19687cc6e67c4972e9764ffeb8f45cdce
gp2112 <me@guip.dev>
2022-08-28 13:43:17 -0300

    delete bind

--- hosts/frostbyte/default.nix
+++ hosts/frostbyte/default.nix
@@ -32,7 +32,7 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
   security.pam.services.swaylock = {};
-  networking.firewall.allowedTCPPortRanges = [ 
+  networking.firewall.allowedTCPPortRanges = [
     {from = 1714; to = 1764;} # kde connect
   ];
   networking.firewall.allowedUDPPortRanges = [
