8a95308d719451470b399fd847760b45f3a8ed63
gp2112 <me@guip.dev>
2022-08-30 10:16:44 -0300

    starship prolog symbol

--- home/gui/common/fish/starship.nix
+++ home/gui/common/fish/starship.nix
@@ -15,6 +15,8 @@
       rust.symbol = " ";
       lua.symbol = " ";
       package.symbol = " ";
+      aws.disabled = true;
+      perl.disabled = true;
 
       sudo = {
         symbol = "  ";
@@ -28,7 +30,17 @@
           discharging_symbol = " ";
           style = "bold red";
         }
-    ];
+      ];
+
+      custom = {
+        prolog = {
+          detect_extensions = [ "pl" ];
+          symbol = " ";
+          description = "Prolog language.";
+          #command = "swipl --version | grep -E '[0-9]*\.[0-9]*\.[0-9]*' -o";
+        };
+
+      };
 
 
     };
