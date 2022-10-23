e65fe458df456069a4869ae5945210f7d2068d03
gp2112 <me@guip.dev>
2022-10-23 15:35:53 -0300

    sway:firefox private window shortcut

--- home/gui/common/desktops/desktop-env/sway/default.nix
+++ home/gui/common/desktops/desktop-env/sway/default.nix
@@ -79,6 +79,7 @@ in {
         "${super}+Shift+p" = "exec grimshot copy active";
         "${super}+Ctrl+p" = "exec grimshot copy window";
         "${mod}+Shift+w" = "exec firefox";
+        "${mod}+Shift+p" = "exec firefox --private-window";
         "XF86AudioRaiseVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ +5%";
         "XF86AudioLowerVolume" = "exec pactl set-sink-volume @DEFAULT_SINK@ -5%";
         "XF86AudioMute"=" exec pactl set-sink-mute @DEFAULT_SINK@ toggle";
