130f4742fbe6d9d7312cf1708906e82658a52123
gp2112 <me@guip.dev>
2022-10-28 10:49:48 -0300

    fix eduroam

--- hosts/common/wifi.nix
+++ hosts/common/wifi.nix
@@ -22,10 +22,12 @@
         auth = ''
           key_mgmt=WPA-EAP
           identity="@EDUROAM_ID@"
-          password=hash:@EDUROAM_HASHED_PASS@
-          phase1="peaplabel=0"
+          password="@EDUROAM_PASSWORD@"
           phase2="auth=MSCHAPV2"
           domain_suffix_match="semfio.usp.br"
+          eap=PEAP
+          auth_alg=OPEN
+          pairwise=CCMP
         '';
         priority=1;
       };
