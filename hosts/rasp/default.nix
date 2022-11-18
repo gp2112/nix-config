9ec1f8002a12eb6df8da50f3086e49ddefa7d36b
gp2112 <me@guip.dev>
2022-11-18 10:25:50 -0300

    rasp:host mastodon

--- hosts/rasp/default.nix
+++ hosts/rasp/default.nix
@@ -11,9 +11,9 @@
 	    ./deluge.nix
 	    ./nginx
         ./git
-        ./wireguard.nix
+        # ./wireguard.nix
         ./nextcloud
-        # ./postgresql.nix
+        ./mastodon
 	];
 
   fileSystems = {
@@ -28,6 +28,7 @@
   };
   networking = {
     hostName = "rasp";
+    useDHCP = false;
 
     nameservers = [ "8.8.8.8" "1.1.1.1" ];
     defaultGateway = {
@@ -35,10 +36,16 @@
       interface = "wlan0";
     };
 
-    interfaces.wlan0.ipv6.addresses = [{
-      address = "2804:2894:f0d1:9400::4:1";
-      prefixLength = 64;
-    }];
+    interfaces.wlan0 = {
+      ipv4.addresses = [{
+        address = "192.168.1.2";
+        prefixLength = 24;
+      }];
+      ipv6.addresses = [{
+        address = "2804:2894:f0d1:9400::4:1";
+        prefixLength = 64;
+      }];
+    };
 
     firewall = {
       enable = true;
