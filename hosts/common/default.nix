8fb550da6331db876ee5a370c32114e1fb84c106
gp2112 <me@guip.dev>
2023-01-20 15:15:37 -0300

    ssh fix settings names

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -30,8 +30,10 @@
 
   services.openssh = {
     enable = true;
-    permitRootLogin = "no";
-    passwordAuthentication = false;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = false;
+    };
   };
 
   programs.ssh.extraConfig = ''
