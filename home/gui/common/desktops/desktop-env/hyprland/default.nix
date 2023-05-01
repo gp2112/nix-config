62a71e154dcd09a20209e1c69dba6108c1a21be9
gp2112 <me@guip.dev>
2023-05-01 19:40:46 -0300

    hyprland: config

--- home/gui/common/desktops/desktop-env/hyprland/default.nix
+++ home/gui/common/desktops/desktop-env/hyprland/default.nix
@@ -12,136 +12,262 @@
     xdragon
   ];
 
-  wayland.windowManager.hyprland = {
+  wayland.windowManager.hyprland = 
+
+  let
+    TERMINAL = "kitty";
+    BROWSER = "firefox";
+    EDITOR = "vim";
+    wallpaper = "~/Pictures/swaybg.png";
+  in {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.default;
     extraConfig = ''
-      monitor=,1920x1080@60,0x0,1
-
-      $lock = echo ig #swaylock --screenshots --clock --effect-blur 5x6
-      $pwrdialog = zenity --info --text="Suspend (s), Hibernate(h), Lock(l) or Shutdown (shift+s)"
-      $reset = hyprctl dispatch submap reset
-      $pwrmode = hyprctl dispatch submap powermgt
-
-
-      exec=swaybg -i /home/gui/Pictures/swaybg.png &
-      exec-once=waybar &
-
-      input {
-          kb_layout=br
-          kb_variant=abnt2
-          kb_model=
-          kb_options=
-          kb_rules=
-
-          follow_mouse=1
-      }
-
       general {
-          sensitivity=1.0 # for mouse cursor
-          main_mod=SUPER
-
-          gaps_in=5
-          gaps_out=20
-          border_size=2
-          col.active_border=0x66ee1111
-          col.inactive_border=0x66333333
-
-          apply_sens_to_raw=0 # whether to apply the sensitivity to raw input (e.g. used by games where you aim using your mouse)
-
-          damage_tracking=full # leave it on full unless you hate your GPU and want to make it suffer
+        gaps_in=15
+        gaps_out=20
+        border_size=2.7
+        cursor_inactive_timeout=4
       }
 
       decoration {
-          rounding=5
-          blur=1
-          blur_size=1 # minimum 1
-          blur_passes=2 # minimum 1, more passes = more resource intensive.
-          # Your blur "amount" is blur_size * blur_passes, but high blur_size (over around 5-ish) will produce artifacts.
-          # if you want heavy blur, you need to up the blur_passes.
-          # the more passes, the more you can up the blur_size without noticing artifacts.
-          active_opacity=0.82
-          inactive_opacity=0.7
+        active_opacity=0.94
+        inactive_opacity=0.84
+        fullscreen_opacity=1.0
+        rounding=5
+        blur=true
+        blur_size=5
+        blur_passes=3
+        blur_new_optimizations=true
+        blur_ignore_opacity=true
+        drop_shadow=true
+        shadow_range=12
+        shadow_offset=3 3
+        col.shadow=0x44000000
+        col.shadow_inactive=0x66000000
       }
 
       animations {
-          enabled=1
-          animation=windows,1,7,default
-          animation=borders,1,10,default
-          animation=fadein,1,10,default
-          animation=workspaces,1,6,default
+        enabled=true
+
+        bezier=easein,0.11, 0, 0.5, 0
+        bezier=easeout,0.5, 1, 0.89, 1
+        bezier=easeinout,0.45, 0, 0.55, 1
+
+        animation=windowsIn,1,3,easeout,slide
+        animation=windowsOut,1,3,easein,slide
+        animation=windowsMove,1,3,easeout
+
+        animation=fadeIn,1,3,easeout
+        animation=fadeOut,1,3,easein
+        animation=fadeSwitch,1,3,easeout
+        animation=fadeShadow,1,3,easeout
+        animation=fadeDim,1,3,easeout
+        animation=border,1,3,easeout
+
+        animation=workspaces,1,2,easeout,slide
       }
 
       dwindle {
-          pseudotile=0 # enable pseudotiling on dwindle
+        split_width_multiplier=1.35
       }
 
       misc {
-        disable_hyprland_logo=1
+        vfr=on
       }
 
-      # example window rules
-      # for windows named/classed as abc and xyz
-      #windowrule=move 69 420,abc
-      #windowrule=size 420 69,abc
-      #windowrule=tile,xyz
-      #windowrule=float,abc
-      #windowrule=pseudo,abc
-      #windowrule=monitor 0,xyz
+      input {
+        kb_layout=br,us
+        touchpad {
+          disable_while_typing=false
+        }
+      }
 
-      monitor=HDMI-1,1920x1080@60,1920x0,1
-
-      submap=powermgt
-      bind=,s,exec,$reset && $lock & systemctl suspend
-      bind=,h,exec,$reset && systemctl hibernate
-      bind=,l,exec,$reset && $lock
-      bind=SHIFT,s,exec,$reset && shutdown -P now
-
-      bind=,escape,submap,reset
-
+      # Passthrough mode (e.g. for VNC)
+      bind=SUPER,P,submap,passthrough
+      submap=passthrough
+      bind=SUPER,P,submap,reset
       submap=reset
 
-      bind=ALT,S,exec,$pwrdialog & $pwrmode
+
+      # Startup
+      exec-once=waybar
+      exec=swaybg -i ${wallpaper} --mode fill
+      exec-once=mako
+      exec-once=swayidle -w
+
+      # Mouse binding
+      bindm=SUPER,mouse:272,movewindow
+      bindm=SUPER,mouse:273,resizewindow
+
+      # Program bindings
+      bind=SUPER,Return,exec,${TERMINAL}
+      bind=SUPER,w,exec,makoctl dismiss
+      bind=SUPER,v,exec,${TERMINAL} $SHELL -ic ${EDITOR}
+      bind=SUPER,b,exec,${BROWSER}
+
+      bind=SUPER,x,exec,wofi -S drun -x 10 -y 10 -W 25% -H 60%
+      bind=SUPER,d,exec,wofi -S run
+      bind=,Scroll_Lock,exec,pass-wofi # fn+k
+      bind=,XF86Calculator,exec,pass-wofi # fn+f12
+
+      # Toggle waybar
+      bind=,XF86Tools,exec,pkill -USR1 waybar # profile button
+
+      # Lock screen
+      bind=,XF86Launch5,exec,swaylock -S
+      bind=,XF86Launch4,exec,swaylock -S
+      bind=SUPER,backspace,exec,swaylock -S
+
+      # Screenshots
+      bind=,Print,exec,grimblast --notify copy output
+      bind=SHIFT,Print,exec,grimblast --notify copy active
+      bind=CONTROL,Print,exec,grimblast --notify copy screen
+      bind=SUPER,Print,exec,grimblast --notify copy window
+      bind=ALT,Print,exec,grimblast --notify copy area
+
+      # Keyboard controls (brightness, media, sound, etc)
+      bind=,XF86MonBrightnessUp,exec,light -A 10
+      bind=,XF86MonBrightnessDown,exec,light -U 10
+
+      bind=,XF86AudioNext,exec,playerctl next
+      bind=,XF86AudioPrev,exec,playerctl previous
+      bind=,XF86AudioPlay,exec,playerctl play-pause
+      bind=,XF86AudioStop,exec,playerctl stop
+      bind=ALT,XF86AudioNext,exec,playerctld shift
+      bind=ALT,XF86AudioPrev,exec,playerctld unshift
+      bind=ALT,XF86AudioPlay,exec,systemctl --user restart playerctld
+
+      bind=,XF86AudioRaiseVolume,exec,pactl set-sink-volume @DEFAULT_SINK@ +5%
+      bind=,XF86AudioLowerVolume,exec,pactl set-sink-volume @DEFAULT_SINK@ -5%
+      bind=,XF86AudioMute,exec,pactl set-sink-mute @DEFAULT_SINK@ toggle
+
+      bind=SHIFT,XF86AudioMute,exec,pactl set-source-mute @DEFAULT_SOURCE@ toggle
+      bind=,XF86AudioMicMute,exec,pactl set-source-mute @DEFAULT_SOURCE@ toggle
 
 
-      # example binds
-      bind=SUPER,RETURN,exec,kitty
+      # Window manager controls
+      bind=SUPERSHIFT,q,killactive
+      bind=SUPERSHIFT,e,exit
 
-      # programs shortcut
-      bind=SHIFTSUPER,W,exec,firefox
+      bind=SUPER,s,togglesplit
+      bind=SUPER,f,fullscreen,1
+      bind=SUPERSHIFT,f,fullscreen,0
+      bind=SUPERSHIFT,space,togglefloating
 
-      bind=SHIFTSUPER,Q,killactive,
-      bind=SUPER,M,exit,
-      bind=SUPER,E,exec,dolphin
-      bind=SUPER,V,togglefloating,
-      bind=SUPER,R,exec,wofi --show drun -o DP-3
-      bind=SUPER,P,pseudo,
+      bind=SUPER,minus,splitratio,-0.25
+      bind=SUPERSHIFT,minus,splitratio,-0.3333333
+
+      bind=SUPER,equal,splitratio,0.25
+      bind=SUPERSHIFT,equal,splitratio,0.3333333
+
+      bind=SUPER,g,togglegroup
+      bind=SUPER,apostrophe,changegroupactive,f
+      bind=SUPERSHIFT,apostrophe,changegroupactive,b
 
       bind=SUPER,left,movefocus,l
       bind=SUPER,right,movefocus,r
       bind=SUPER,up,movefocus,u
       bind=SUPER,down,movefocus,d
+      bind=SUPER,h,movefocus,l
+      bind=SUPER,l,movefocus,r
+      bind=SUPER,k,movefocus,u
+      bind=SUPER,j,movefocus,d
 
-      bind=SUPER,1,workspace,1
-      bind=SUPER,2,workspace,2
-      bind=SUPER,3,workspace,3
-      bind=SUPER,4,workspace,4
-      bind=SUPER,5,workspace,5
-      bind=SUPER,6,workspace,6
-      bind=SUPER,7,workspace,7
-      bind=SUPER,8,workspace,8
-      bind=SUPER,9,workspace,9
+      bind=SUPERSHIFT,left,movewindow,l
+      bind=SUPERSHIFT,right,movewindow,r
+      bind=SUPERSHIFT,up,movewindow,u
+      bind=SUPERSHIFT,down,movewindow,d
+      bind=SUPERSHIFT,h,movewindow,l
+      bind=SUPERSHIFT,l,movewindow,r
+      bind=SUPERSHIFT,k,movewindow,u
+      bind=SUPERSHIFT,j,movewindow,d
+
+      bind=SUPERCONTROL,left,focusmonitor,l
+      bind=SUPERCONTROL,right,focusmonitor,r
+      bind=SUPERCONTROL,up,focusmonitor,u
+      bind=SUPERCONTROL,down,focusmonitor,d
+      bind=SUPERCONTROL,h,focusmonitor,l
+      bind=SUPERCONTROL,l,focusmonitor,r
+      bind=SUPERCONTROL,k,focusmonitor,u
+      bind=SUPERCONTROL,j,focusmonitor,d
+
+      bind=SUPERCONTROL,1,focusmonitor,DP-1
+      bind=SUPERCONTROL,2,focusmonitor,DP-2
+      bind=SUPERCONTROL,3,focusmonitor,DP-3
+
+      bind=SUPERCONTROLSHIFT,left,movewindow,mon:l
+      bind=SUPERCONTROLSHIFT,right,movewindow,mon:r
+      bind=SUPERCONTROLSHIFT,up,movewindow,mon:u
+      bind=SUPERCONTROLSHIFT,down,movewindow,mon:d
+      bind=SUPERCONTROLSHIFT,h,movewindow,mon:l
+      bind=SUPERCONTROLSHIFT,l,movewindow,mon:r
+      bind=SUPERCONTROLSHIFT,k,movewindow,mon:u
+      bind=SUPERCONTROLSHIFT,j,movewindow,mon:d
+
+      bind=SUPERCONTROLSHIFT,1,movewindow,mon:DP-1
+      bind=SUPERCONTROLSHIFT,2,movewindow,mon:DP-2
+      bind=SUPERCONTROLSHIFT,3,movewindow,mon:DP-3
+
+      bind=SUPERALT,left,movecurrentworkspacetomonitor,l
+      bind=SUPERALT,right,movecurrentworkspacetomonitor,r
+      bind=SUPERALT,up,movecurrentworkspacetomonitor,u
+      bind=SUPERALT,down,movecurrentworkspacetomonitor,d
+      bind=SUPERALT,h,movecurrentworkspacetomonitor,l
+      bind=SUPERALT,l,movecurrentworkspacetomonitor,r
+      bind=SUPERALT,k,movecurrentworkspacetomonitor,u
+      bind=SUPERALT,j,movecurrentworkspacetomonitor,d
+
+      bind=SUPER,u,togglespecialworkspace
+      bind=SUPERSHIFT,u,movetoworkspace,special
+
+      bind=SUPER,1,workspace,01
+      bind=SUPER,2,workspace,02
+      bind=SUPER,3,workspace,03
+      bind=SUPER,4,workspace,04
+      bind=SUPER,5,workspace,05
+      bind=SUPER,6,workspace,06
+      bind=SUPER,7,workspace,07
+      bind=SUPER,8,workspace,08
+      bind=SUPER,9,workspace,09
       bind=SUPER,0,workspace,10
+      bind=SUPER,f1,workspace,11
+      bind=SUPER,f2,workspace,12
+      bind=SUPER,f3,workspace,13
+      bind=SUPER,f4,workspace,14
+      bind=SUPER,f5,workspace,15
+      bind=SUPER,f6,workspace,16
+      bind=SUPER,f7,workspace,17
+      bind=SUPER,f8,workspace,18
+      bind=SUPER,f9,workspace,19
+      bind=SUPER,f10,workspace,20
+      bind=SUPER,f11,workspace,21
+      bind=SUPER,f12,workspace,22
 
-      bind=SHIFTSUPER,exclam,movetoworkspace,1
-      bind=SHIFTSUPER,at,movetoworkspace,2
-      bind=SHIFTSUPER,numbersign,movetoworkspace,3
-      bind=SHIFTSUPER,dollar,movetoworkspace,4
-      bind=SHIFTSUPER,percent,movetoworkspace,5
-      bind=SHIFTSUPER,asciicircum,movetoworkspace,6
-      bind=SHIFTSUPER,ampersand,movetoworkspace,7
-      bind=SHIFTSUPER,asterisk,movetoworkspace,8
-      bind=SHIFTSUPER,parentleft,movetoworkspace,9
-      bind=SHIFTSUPER,parentright,movetoworkspace,10'';
+      bind=SUPERSHIFT,1,movetoworkspacesilent,01
+      bind=SUPERSHIFT,2,movetoworkspacesilent,02
+      bind=SUPERSHIFT,3,movetoworkspacesilent,03
+      bind=SUPERSHIFT,4,movetoworkspacesilent,04
+      bind=SUPERSHIFT,5,movetoworkspacesilent,05
+      bind=SUPERSHIFT,6,movetoworkspacesilent,06
+      bind=SUPERSHIFT,7,movetoworkspacesilent,07
+      bind=SUPERSHIFT,8,movetoworkspacesilent,08
+      bind=SUPERSHIFT,9,movetoworkspacesilent,09
+      bind=SUPERSHIFT,0,movetoworkspacesilent,10
+      bind=SUPERSHIFT,f1,movetoworkspacesilent,11
+      bind=SUPERSHIFT,f2,movetoworkspacesilent,12
+      bind=SUPERSHIFT,f3,movetoworkspacesilent,13
+      bind=SUPERSHIFT,f4,movetoworkspacesilent,14
+      bind=SUPERSHIFT,f5,movetoworkspacesilent,15
+      bind=SUPERSHIFT,f6,movetoworkspacesilent,16
+      bind=SUPERSHIFT,f7,movetoworkspacesilent,17
+      bind=SUPERSHIFT,f8,movetoworkspacesilent,18
+      bind=SUPERSHIFT,f9,movetoworkspacesilent,19
+      bind=SUPERSHIFT,f10,movetoworkspacesilent,20
+      bind=SUPERSHIFT,f11,movetoworkspacesilent,21
+      bind=SUPERSHIFT,f12,movetoworkspacesilent,22
+
+      blurls=waybar
+    '';
   };
 }
