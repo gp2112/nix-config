4853632599761103422481aab3b84b6883677574
gp2112 <me@guip.dev>
2023-07-04 21:57:30 -0300

    proton: add pipewire

--- hosts/proton/hardware.nix
+++ hosts/proton/hardware.nix
@@ -41,4 +41,19 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
+
+  users.users.gui.extraGroups = [ "jackaudio" ];
+
+
+  # Enable pipewire sound.
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [ libjack2 jack2 qjackctl jack_capture ];
+
 }
