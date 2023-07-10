24280c0df69bc1fc2701d293be21697998def367
gp2112 <me@guip.dev>
2023-07-10 14:58:18 -0300

    proton: fix networking

--- hosts/proton/default.nix
+++ hosts/proton/default.nix
@@ -15,21 +15,20 @@
   networking = {
     hostName = "proton";
 
-    nameservers = [ "8.8.8.8" "8.8.4.4" ];
+    nameservers = [ "143.107.231.12" ];
+
 
     interfaces = {
       eno1 = {
-        ipv4 = {
-          addresses = [{
-            address = "143.104.231.209";
-            prefixLength = 26;
-          }];
-          routes = [
-            { address = "0.0.0.0"; prefixLength = 0; via = "143.107.231.193"; }
-          ];
-        };
+        useDHCP = false;
       };
     };
+
+    localCommands = ''
+      ip address add 143.107.231.209/26 brd + dev eno1
+      ip route add 143.107.231.193 dev eno1
+      ip route add default via 143.107.231.193 dev eno1
+    '';
 
   };
 
