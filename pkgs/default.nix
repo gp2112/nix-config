c96a00c927b35550dd737d77dff767058c21fdf7
Guilherme Ramos Costa Paixao <guircp@usp.br>
2023-07-13 09:39:33 -0300

    pkgs: json-prettify

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,22 @@
 # When you add custom packages, list them here
 { pkgs }: {
   netkit = pkgs.callPackage ./netkit { };
+  json-prettify = (pkgs.writeScriptBin "json-prettify" ''
+      #!${pkgs.python3}/bin/python
+      import json
+      import sys
+
+      def main():
+          try:
+              fname = sys.argv[1]
+              f = open(fname)
+          except IndexError:
+              f = sys.stdin
+          
+          data = json.load(f)
+          print(json.dumps(data, indent=4))
+
+      if __name__ == '__main__':
+          main()
+    '');
 }
