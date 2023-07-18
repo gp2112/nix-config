e2466ccd75951912adf7e10f7afe1321fcd55905
Guilherme Ramos Costa Paixao <guircp@usp.br>
2023-07-18 14:50:51 -0300

    home: add nvim fugitive

--- home/gui/common/nvim/default.nix
+++ home/gui/common/nvim/default.nix
@@ -7,30 +7,30 @@
   programs.neovim = {
     enable = true;
     extraConfig = ''
-        set nocompatible           
-        set showmatch               
-        set ignorecase            
-        set mouse=a                 
-        set hlsearch                
-        set incsearch               
-        set tabstop=4               
-        set softtabstop=4           
-        set expandtab               
-        set shiftwidth=4            
-        set autoindent              
-        set number                  
-        set wildmode=longest,list   
-        set cc=80                  
-        filetype plugin indent on   
-        syntax on                   
-        set mouse=a                 
-        set clipboard+=unnamedplus  
+        set nocompatible
+        set showmatch
+        set ignorecase
+        set mouse=a
+        set hlsearch
+        set incsearch
+        set tabstop=4
+        set softtabstop=4
+        set expandtab
+        set shiftwidth=4
+        set autoindent
+        set number
+        set wildmode=longest,list
+        set cc=80
+        filetype plugin indent on
+        syntax on
+        set mouse=a
+        set clipboard+=unnamedplus
         filetype plugin on
-        set cursorline              
-        set ttyfast                 
-        set spell                 
+        set cursorline
+        set ttyfast
+        set spell
         set noswapfile
-        colorscheme terafox 
+        colorscheme terafox
         set conceallevel=2
         map <C-n> :NERDTreeToggle<CR>
         '';
@@ -44,6 +44,7 @@
     vim-surround
     nerdtree
     nvim-autopairs
+    fugitive
   ];
 
   defaultEditor = true;
