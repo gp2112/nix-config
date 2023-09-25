0fd2b9bd338bd17dc3b085b9f27a18ea3ccde540
gp2112 <me@guip.dev>
2023-09-25 13:23:44 -0300

    deluge: manage connections limit

--- hosts/rasp/deluge.nix
+++ hosts/rasp/deluge.nix
@@ -11,6 +11,9 @@
 	    allow_remote = true;
 	    daemon_port = 58846;
 	    listen_ports = [ 6881 6889 ];
+        max_upload_slots_global = 30;
+        max_active_seeding = 30;
+        max_active_limit = 40;
     };
     web.enable = true;
     openFirewall = true;
